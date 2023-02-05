50978b8e9baded448018002ae01d9eaa4f1cc1d4
Chris Lane <lanec@pacificu.edu>
2023-02-04 16:06:17 -0800

    Added driver, .h and .cpp for DNAStrand

---
+++ DNAStrand/DNAStrand.h
@@ -0,0 +1,44 @@
+#pragma once
+//********************************************************************* 
+// File name:		DNAStrand.h
+// Author:			CS, Pacific University
+// Date:				
+// Class:				CS 250
+// Assignment:  01 DNA Class
+// Purpose:			Declare the interface for a strand of DNA. A strand of
+//							DNA contains a unique and an unknow number of chemical
+//							bases adenine (A), guanine (G), cytosine (C), and 
+//							thymine (T). The ID will be a simplified FASTA string.
+//*********************************************************************
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class DNAStrand {
+
+public:
+	DNAStrand(const string& rcBases = "");
+	DNAStrand(const DNAStrand& rcDNAStrand);
+
+	char getBase(int whichBase) const;
+
+	bool read(istream& rcInStream);
+	void write(ostream& rcOutStream) const;
+
+	DNAStrand reverse() const;
+	DNAStrand complement() const;
+	double gcContent() const;
+	double hamming(const DNAStrand& rcDNAStrand) const;
+
+	unsigned int size() const;
+	bool isEqual(const DNAStrand& rcDNAStrand) const;
+	bool isLegalStrand() const;
+
+private:
+	char INVALID_BASE = '!';
+	string mBases;
+
+	char dnaBaseComplement(char base) const;
+};
